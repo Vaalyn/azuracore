19c3872875f3f452b7d554d19555d402c2a313ef
Buster Neece <loobalightdark@gmail.com>
2019-03-07 19:38:02 -0600

    Allow direct setting of the formatted message on all Azura Exceptions.

--- src/Exception.php
+++ src/Exception.php
@@ -14,43 +14,83 @@ class Exception extends \Exception
     /** @var array Additional data supplied to the logger class when handling the exception. */
     protected $logging_context = [];
 
-    public function setMessage($message)
+    /** @var string|null */
+    protected $formatted_message;
+
+    /**
+     * @param string $message
+     */
+    public function setMessage($message): void
     {
         $this->message = $message;
     }
 
-    public function getFormattedMessage(): string
+    /**
+     * Set a display-formatted message (if one exists).
+     *
+     * @param string|null $message
+     */
+    public function setFormattedMessage($message): void
     {
-        return $this->message;
+        $this->formatted_message = $message;
     }
 
+    /**
+     * @return string A display-formatted message, if one exists, or
+     *                the regular message if one doesn't.
+     */
+    public function getFormattedMessage(): string
+    {
+        return $this->formatted_message ?? $this->message;
+    }
+
+    /**
+     * @param int $logger_level
+     */
     public function setLoggerLevel($logger_level): void
     {
         $this->logger_level = $logger_level;
     }
 
+    /**
+     * @return int
+     */
     public function getLoggerLevel(): int
     {
         return $this->logger_level;
     }
 
-    public function addExtraData($legend, $data)
+    /**
+     * @param string|int $legend
+     * @param mixed $data
+     */
+    public function addExtraData($legend, $data): void
     {
         if (is_array($data)) {
             $this->extra_data[$legend] = $data;
         }
     }
 
+    /**
+     * @return array
+     */
     public function getExtraData(): array
     {
         return $this->extra_data;
     }
 
-    public function addLoggingContext($key, $data)
+    /**
+     * @param string|int $key
+     * @param mixed $data
+     */
+    public function addLoggingContext($key, $data): void
     {
         $this->logging_context[$key] = $data;
     }
 
+    /**
+     * @return array
+     */
     public function getLoggingContext(): array
     {
         return $this->logging_context;
